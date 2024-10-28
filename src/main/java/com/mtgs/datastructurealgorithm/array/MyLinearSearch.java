94155f699a7b067e8caa1ff104c8fba79a48dd3a
May Thagyan <may.thagyan@privasia.com>
2024-10-28 16:17:17 +0800

    fix log

--- src/main/java/com/mtgs/datastructurealgorithm/array/MyLinearSearch.java
+++ src/main/java/com/mtgs/datastructurealgorithm/array/MyLinearSearch.java
@@ -24,11 +24,10 @@ public class MyLinearSearch extends Array implements Searchable {
         Util.printIndex("My Linear Search Item ", item);
         for (int i = 0; i < items.length; i++) {
             if (items[i] == item) {
-                Util.printIndex("My Linear Search i ", i);
+                Util.printIndex("My Linear Search found i ", i);
                 return i;
-
             } else if (isSorted && items[i] > item) {
-                Util.printIndex("My Linear Search i ", i);
+                Util.printIndex("My Linear Search not found i ", i);
                 return -1;
             } else {
                 Util.printIndex("My Linear Search i ", i);
